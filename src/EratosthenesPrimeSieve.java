437ec3953084fcc6a930479ee11ac31468d84b9e
gschwendtnerj <gschwendtnerj190088@sus.htl-grieskirchen.at>
2021-09-20 20:14:21 +0200

    initial commit

---
+++ src/EratosthenesPrimeSieve.java
@@ -0,0 +1,20 @@
+public class EratosthenesPrimeSieve implements PrimeSieve {
+
+    private int upperLimit;
+
+    public EratosthenesPrimeSieve(int upperLimit)
+    {
+        this.upperLimit = upperLimit;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+
+}
